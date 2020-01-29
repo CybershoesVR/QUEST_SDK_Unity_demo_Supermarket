71a668212b2fdf1c479eeb22aedcb0981939a429
Michael Bieglmayer <mail@cybershoes.io>
2020-01-29 17:11:11 +0100

    Added Supermarket Asset

---
+++ CyberRunnerForQuest/Assets/Project/Scripts/BlockShooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class BlockShooter : MonoBehaviour
+{
+    [SerializeField] GameObject[] projectiles;
+    [SerializeField] float speed = 500;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] OVRInput.Axis1D trigger;
+
+    private bool lastPressed = false;
+
+
+    void Update()
+    {
+        if (OVRInput.Get(trigger) > 0 && !lastPressed)
+        {
+            lastPressed = true;
+
+            int projectileindex = Random.Range(0, projectiles.Length);
+
+            GameObject bullet = Instantiate(projectiles[projectileindex], spawnPoint.position, spawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * speed);
+        }
+        else if (OVRInput.Get(trigger) <= 0)
+        {
+            lastPressed = false;
+        }
+    }
+}
